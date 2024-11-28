31af8204cbb69c43f720319cc974870b5b5cc809
Hp <sunlight847.moon@gmail.com>
2024-11-28 20:52:52 +0500

    second

--- new/settings.py
+++ new/settings.py
@@ -77,13 +77,22 @@ WSGI_APPLICATION = 'new.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    'default' :{
+        'ENGINE' : 'django.db.backends.sqlite3',
+        'NAME': 'news',
+        'USER' : 'postgres',
+        'PORT': 5432,
+        'HOST' : 'localhost',
+        'PASSWORD' : 'moon_walker7'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- user/views.py
+++ user/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
+
 
 from user.forms import UserLoginForm, UserRegisterForm
 
